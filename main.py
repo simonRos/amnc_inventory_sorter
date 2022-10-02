e1024aae5491d5eab81f28e04cb27ed9203575cb
simonRos <simon.h.rosner@gmail.com>
2022-10-01 22:20:04 -0400

    removed debug outputs

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ from methods import *
 def main():
     try:
         create_db()
-        print("db created")
         load_inventory()
-        print("inventory loaded")
         load_top_inventory()
-        print("top loaded")
     except ValueError as ve:
         return str(ve)
 
